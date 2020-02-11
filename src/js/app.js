93b620b0ca58b9e12d8a4fce0478ccbe78642755
d-kacprzak <d.kacprzak@hotmail.com>
2020-02-11 15:56:18 +0100

    Adding content to displaying repo ->app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import { clientID, clientSecret } from './APP_KEY';
 
 $(document).ready(() => {
-  $('#button-submit').on('click', function (e) {
+  const desc = ' No description';
+
+  $('#button-submit').on('click', (e) => {
     e.preventDefault();
     const searchValue = $('#search').val();
 
@@ -13,13 +15,49 @@ $(document).ready(() => {
         clientSecret,
       },
     }).done((user) => {
-        console.log(user)
+      $.ajax({
+        url: `https://api.github.com/users/${searchValue}/repos`,
+        data: {
+          clientID,
+          clientSecret,
+          sort: 'created asc',
+          per_page: 10,
+        },
+      }).done((repos) => {
+        $.each(repos, (index, val) => {
+          $('#repos').append(`
+            <div class="card  mb-2 p-2">
+                <div class="row col-12">
+                    <div>
+                        <strong>${val.name}</strong>
+                        <span class="badge badge-info m-1 p-1">Forks: ${val.forks_count}</span>
+                        <span class="badge badge-warning m-1 p-1">Watchers: ${val.watchers_count}</span>
+                        <span class="badge badge-primary m-1 p-1">Stars: ${val.stargazers_count}</span>
+                        <span class="badge badge-danger m-1 p-1">language: ${val.language}</span>
+                        <br>
+                        
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-9">
+                        ${(val.description) ? val.description : desc}
+                    </div>
+                    <div class="col-md-3 h-25">
+                        <a target="_blank" class="btn btn-outline-info" href="${val.html_url}">View Repo</a>
+                     </div>
+                </div>
+                
+            </div>
+        
+              `);
+        });
+      });
       $('#result').html(`
-        <div class="card mr-2" style="width: 15rem;">
+        <div class="card mr-2 mb-2 h-100" style="width: 15rem;">
           <img src="${user.avatar_url}" class="card-img-top" alt="${user.avatar_url}">
           <div class="card-body">
             <h5 class="card-title">${user.name}</h5>
-            <p class="card-text">${(user.bio) ? user.bio : 'no description'}</p>
+            <p class="card-text">${(user.bio) ? user.bio : desc}</p>
           </div>
         
           <div class="card-body">
@@ -30,18 +68,28 @@ $(document).ready(() => {
         
         <div class="card col-md-9" style="width: 15rem;">
           <div class="row m-2">
-            <span class="badge badge-info m-1 p-1">Public Repos: ${user.public_repos}</span>
-            <span class="badge badge-warning m-1 p-1">Public Gist: ${user.public_gists}</span>
-            <span class="badge badge-primary m-1 p-1">Followers: ${user.followers}</span>
+            <span class="badge badge-info m-1 p-2">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-warning m-1 p-2">Public Gist: ${user.public_gists}</span>
+            <span class="badge badge-primary m-1 p-2">Followers: ${user.followers}</span>
           </div>  
           
           <ul class="list-group">
-            <li class="list-group-item"></li>
-            <li class="list-group-item"></li>
-            <li class="list-group-item"></li>
+            <li class="list-group-item">Company: ${(user.company) ? user.company : desc}</li>
+            <li class="list-group-item">Location: ${(user.location) ? user.location : desc}</li>
+            <li class="list-group-item">Member Since: ${(user.created_at) ? user.created_at : desc}</li>
           </ul>
+          <h5 class="page-header mt-3">Latest Repos:</h5>
+          <div id="repos"></div>
         </div>
+      
       `);
+        ($('#footer').empty()) ? $('#footer').append(`
+        <!-- Copyright -->
+        <div class="footer footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="#"> Damian Kacprzak</a>
+        </div>
+        <!-- Copyright -->
+      `) : null;
     });
   });
 });
